1ec52c6308df0776732c40c0fcf1dc1b0a20744f
xkawi <kawixiao@gmail.com>
2016-09-18 13:29:52 +0800

    replace now-serve with micro-list
    This add the ability to deploy using Now CLI directly with extra parameters such as env variables, and serve the production build using micro-list.

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 
 class App extends Component {
   render() {
+    const secretCode = process.env.REACT_APP_SECRET_CODE;
     return (
       <div className="App">
         <div className="App-header">
@@ -13,6 +14,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>REACT_APP_SECRET_CODE: { !secretCode ? 'env not set yet' : secretCode }</p>
       </div>
     );
   }
