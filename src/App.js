64dfb0dee4a69afc92f76bd72d0b147a8a036aed
Kawi Xiao <kawixiao@gmail.com>
2017-03-11 14:27:02 +0800

    use react-router v4

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  Switch,
+} from 'react-router-dom'
+import About from './About';
 import logo from './logo.svg';
 import './App.css';
+
+const NoMatch = ({ location }) => (
+  <div>
+    <p>No Match for <code>{location.pathname}</code></p>
+    <Link to='/'>go back</Link>
+  </div>
+)
 
 class App extends Component {
   render() {
@@ -17,10 +30,21 @@ class App extends Component {
         </p>
         <p>REACT_APP_SECRET_CODE: { !secretCode ? 'env not set yet' : secretCode }</p>
         <p><Link to='/about'>about us</Link></p>
-        {this.props.children}
       </div>
     );
   }
 }
 
-export default App;
+const BasicApp = () => (
+  <Router>
+    <div>
+      <Switch>
+        <Route exact path="/" component={App} />
+        <Route path="/about" component={About} />
+        <Route component={NoMatch} />
+      </Switch>
+    </div>
+  </Router>
+)
+
+export default BasicApp;
--- src/index.js
+++ src/index.js
@@ -1,18 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Router, Route, browserHistory, Link } from 'react-router'
 import App from './App';
-import About from './About';
 import './index.css';
 
-const NoMatch = () => (
-  <p>No Match <Link to='/'>go back</Link></p>
-)
-
-ReactDOM.render((
-  <Router history={browserHistory} >
-    <Route path="/" component={App} />
-    <Route path="/about" component={About}/>
-    <Route path="*" component={NoMatch}/>
-  </Router>
-), document.getElementById('root'));
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
